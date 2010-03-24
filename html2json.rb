290a6436dab84a0d423e5c35135ebca46a6683bb
lizconlan <Liz@liz-conlans-macbook.local>
2010-03-24 16:47:49 +0000

    rejecting incorrect data, writing failures to log file

--- html2json.rb
+++ html2json.rb
@@ -6,58 +6,128 @@ require 'rest_client'
 DBSERVER = "http://localhost:5984"
 DATABASE = "#{DBSERVER}/divisionlists"
 
-Dir.glob('./data/*.html').each do |html_file|
+def get_uuid
+  result = RestClient.get("#{DBSERVER}/_uuids")
+  values = JSON.parse(result.body)
+  values["uuids"].first
+end
+
+def write_to_log message
+  File.open("./dataload.log", 'a') {|f| f.write(message) }
+end
+
+def log_error current_file, ref, item_name, item
+  message = "error in file #{current_file}#{ref} - #{item_name} count was #{item.count}, expected 1\n"
+  write_to_log(message)  
+end
+
+def load_divisions_list html_file
   nokogiri_doc = Nokogiri::HTML(open(html_file))
+  
+  nokogiri_doc.xpath('//div[@class="division-list"]').each do |division_list|
+    error_occured = false
     
-    nokogiri_doc.xpath('//div[@class="division-list"]').each do |division_list|
-      division_hash = {}
+    division_hash = {}
+  
+    current_file = html_file.split("/").last
+    division_hash["file"] = current_file
+  
+    page = division_list.xpath('p[@class="page"]')
+    date = division_list.xpath('p[@class="date"]')
+    number = division_list.xpath('p[@class="divnum"]')
+    resolution = division_list.xpath('p[@class="resolution"]')
+    ayes = division_list.xpath('ol[@class="ayes"]')
+    noes = division_list.xpath('ol[@class="noes"]')
+    
+    error_ref = ""
+    if number.count == 1
+      #we can have more detailed error messages, yay!
+      error_ref = " in divisionlist #{number.text}"
+    elsif page.count == 1
+      #failover reference point
+      error_ref = " referring to page #{page.text}"
+    else
+      #no idea where this happened
+      error_ref = ""
+    end    
+    
+    unless page.count == 1
+      log_error current_file, error_ref, "page", page
+      error_occured = true
+    end
+    
+    unless date.count == 1
+      log_error current_file, error_ref, "date", date
+      error_occured = true
+    end
+    
+    unless number.count == 1
+      log_error current_file, error_ref, "number", number
+      error_occured = true
+    end
+    
+    unless ayes.count == 1
+      log_error current_file, error_ref, "ayes", ayes
+      error_occured = true
+    end
+    
+    unless noes.count == 1
+      log_error current_file, error_ref, "noes", noes
+      error_occured = true
+    end
+    
+    unless resolution.count == 1
+      log_error current_file, error_ref, "resolution", resolution
+      error_occured = true
+    end
+    
+    unless error_occured
+      division_hash["page"] = page.text
+      division_hash["date"] = date.text
+      division_hash["number"] = number.text
+      division_hash["resolution"] = resolution.text
       
-      filename = html_file.split("/").last
-      division_hash["file"] = filename
-      
-      division_hash["page"] = division_list.xpath('p[@class="page"]').text
-      division_hash["date"] = division_list.xpath('p[@class="date"]').text
-      division_hash["number"] = division_list.xpath('p[@class="divnum"]').text
-      division_hash["resolution"] = division_list.xpath('p[@class="resolution"]').text
-            
       ayes_array = []
-      division_list.xpath('ol[@class="ayes"]/li').each do |aye|
+      ayes.xpath('/li').each do |aye|
         ayes_array << aye.text.strip
       end
       division_hash["ayes"] = ayes_array
-            
-      ayes_first = division_list.xpath('ol[@class="ayes"]').first
+      
+      ayes_first = ayes.first
       if ayes_first
         division_hash["ayes_tellers"] = ayes_first.next_sibling().text
       end
-      
+
       noes_array = []
-      division_list.xpath('ol[@class="noes"]/li').each do |noe|
+      noes.xpath('/li').each do |noe|
         noes_array << noe.text.strip
       end
       division_hash["noes"] = noes_array
-      
-      noes_first = division_list.xpath('ol[@class="noes"]').first
+
+      noes_first = noes.first
       if noes_first
         division_hash["noes_tellers"] =  noes_first.next_sibling().text
       end
-      
-      #get the database to generate a valid UUID
-      result = RestClient.get("#{DBSERVER}/_uuids")
-      values = JSON.parse(result.body)
-      uuid = values["uuids"].first
-      
+    
+      uuid = get_uuid()
+
       #convert the hash into a valid JSON doc
       doc = <<-JSON
       #{JSON.generate(division_hash)}
       JSON
-      
+
       #PUT the new record to the database
       RestClient.put("#{DATABASE}/#{uuid}", doc)
-      #optionally could check the result is 201 Created...
     end
-    
+  end
 end
 
+def load_data
+  write_to_log("#{Time.now}\n")
+  Dir.glob('./data/*.html').each do |html_file|
+    load_divisions_list(html_file)
+  end
+  write_to_log("\n")
+end
 
-
+load_data()
