7853b0f9a1d6dd6e5fddf11ee150eb06c10ae1a1
lizconlan <Liz@liz-conlans-macbook.local>
2010-03-25 14:40:35 +0000

    using erb view to output the xml

--- server.rb
+++ server.rb
@@ -2,20 +2,15 @@ require 'rubygems'
 require 'sinatra'
 require 'rest_client'
 require 'json'
+require 'erb'
 
 DB = 'http://localhost:5984/divisionlists'
 
 get '/divisions/number/:numberkey.xml' do
   data = RestClient.get "#{DB}/_design/divisions/_view/by_number?key=%22#{params[:numberkey]}%22"
-    result = JSON.parse(data.body)
-    division = result["rows"].first["value"]
-    content_type 'text/xml', :charset => 'utf-8'
-  %Q{<?xml version="1.0" encoding="UTF-8"?>
-    <division>
-    <h1>#{division["number"]}</h1>
-    <blockquote>#{division["resolution"]}</blockquote>
-<pre>#{division.inspect}</pre>
-</division>
-}
+  result = JSON.parse(data.body)
+  @division = result["rows"].first["value"]
+  content_type 'text/xml', :charset => 'utf-8'
+  erb :numbered_division, :format => :xml
 end
 
