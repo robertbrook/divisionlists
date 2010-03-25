2c17754cddfb178eefadd18686bc25631d2f5b83
Robert Brook <mail@robertbrook.com>
2010-03-25 12:40:04 +0000

    demo code

--- server.rb
+++ server.rb
@@ -5,10 +5,17 @@ require 'json'
 
 DB = 'http://localhost:5984/divisionlists'
 
-get '/divisionlists/' do
-  data = RestClient.get "#{DB}/_design/divisionlists/_view/divisionlists"
-  result = JSON.parse(data)
-  %Q{
-<h1>HELLO</h1>
+get '/divisions/number/:numberkey.xml' do
+  data = RestClient.get "#{DB}/_design/divisions/_view/by_number?key=%22#{params[:numberkey]}%22"
+    result = JSON.parse(data.body)
+    division = result["rows"].first["value"]
+    content_type 'text/xml', :charset => 'utf-8'
+  %Q{<?xml version="1.0" encoding="UTF-8"?>
+    <division>
+    <h1>#{division["number"]}</h1>
+    <blockquote>#{division["resolution"]}</blockquote>
+<pre>#{division.inspect}</pre>
+</division>
 }
 end
+
