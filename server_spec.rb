f883aba012766e4d126d5d98afdcdb5b3904f222
Robert Brook <mail@robertbrook.com>
2010-03-25 16:28:43 +0000

    test placeholder

---
+++ server_spec.rb
@@ -0,0 +1,19 @@
+require 'server'  # <-- your sinatra app
+require 'spec'
+require 'rack/test'
+
+set :environment, :test
+
+describe 'The HelloWorld App' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "says hello" do
+    get '/'
+    last_response.should be_ok
+    last_response.body.should == 'Hello World'
+  end
+end
