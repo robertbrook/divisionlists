5a85f1025771c6cd1d7f4637ca7a25bb84be1428
lizconlan <Liz@liz-conlans-macbook.local>
2010-03-29 14:55:59 +0100

    added link to the Hansard Archive of the date the Division took place; closes gh-12

--- server.rb
+++ server.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'rest_client'
 require 'json'
 require 'erb'
+require 'date'
 
 DB = 'http://localhost:5984/divisionlists'
 
@@ -10,7 +11,11 @@ get '/divisions/number/:numberkey.xml' do
   data = RestClient.get "#{DB}/_design/divisions/_view/by_number?key=%22#{params[:numberkey]}%22"
   result = JSON.parse(data.body)
   @division = result["rows"].first["value"]
+  
+  date = Date.new(@division["numeric_date"][0].to_i, @division["numeric_date"][1].to_i, @division["numeric_date"][2].to_i)
+  hansard_date = date.strftime('%Y/%b/%d').downcase.gsub("/0", "/")
+  @archive_link = "http://hansard.millbanksystems.com/sittings/#{hansard_date}"
+  
   content_type 'text/xml', :charset => 'utf-8'
   erb :numbered_division, :format => :xml
 end
-
