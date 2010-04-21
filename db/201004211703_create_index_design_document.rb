e2d5669104143610e3934dfdf273c01a2b20f0ab
lizconlan <liz@Lizs-MacBook.local>
2010-04-21 17:15:21 +0100

    new design document - first attempt at eyword indexes

---
+++ db/201004211703_create_index_design_document.rb
@@ -0,0 +1,23 @@
+require 'db_helper'
+require 'rubygems'
+require 'rest_client'
+
+doc = '{
+   "_id": "_design/index",
+   "language": "javascript",
+   "views": {
+       "resolution": {
+           "map": "function(doc) {\n  var words_used = \"|\";\n  var tokens;\n  if (doc.resolution) {\n    tokens = doc.resolution.split(/[^A-Z0-9\\-_]+/i);\n    for (word in tokens) {\n      keyword = tokens[word].toLowerCase();\n      if (words_used.indexOf(\"|\" + keyword + \"|\") < 0) {\n        if (keyword.length > 3) {\n          emit(keyword, doc._id);\n          words_used = words_used + keyword + \"|\";\n        }\n      }\n    }\n  }\n}"
+       },
+       "member_name": {
+           "map": "function(doc) {\n  var words_used = \"|\";\n  var tokens;\n  if (doc.noes) {\n    for (voter in doc.noes) {\n      votename = doc.noes[voter].forename + \" \" + doc.noes[voter].surname;\n      tokens = votename.split(/[^A-Z0-9\\-_]+/i);\n      for (word in tokens) {\n        keyword = tokens[word].toLowerCase();\n        if (words_used.indexOf(\"|\" + keyword + \"|\") < 0) {\n          if (keyword.length > 1) {\n            emit(keyword, doc._id);\n            words_used = words_used + keyword + \"|\";\n          }\n        }\n      }\n    }\n  }\n  if (doc.ayes) {\n    for (voter in doc.ayes) {\n      votename = doc.ayes[voter].forename + \" \" + doc.ayes[voter].surname;\n      tokens = votename.split(/[^A-Z0-9\\-_]+/i);\n      for (word in tokens) {\n        keyword = tokens[word].toLowerCase();\n        if (words_used.indexOf(\"|\" + keyword + \"|\") < 0) {\n          if (keyword.length > 1) {\n            emit(keyword, doc._id);\n            words_used = words_used + keyword + \"|\";\n          }\n        }\n      }\n    }\n  }\n}"
+       },
+       "constituency": {
+           "map": "function(doc) {\n  var words_used = \"|\";\n  var tokens;\n  if (doc.noes) {\n    for (voter in doc.noes) {\n      constituency = doc.noes[voter].constituency;\n      tokens = constituency.split(/[^A-Z0-9\\-_]+/i);\n      for (word in tokens) {\n        keyword = tokens[word].toLowerCase();\n        if (words_used.indexOf(\"|\" + keyword + \"|\") < 0) {\n          if (keyword.length > 1) {\n            emit(keyword, doc._id);\n            words_used = words_used + keyword + \"|\";\n          }\n        }\n      }\n    }\n  }\n  if (doc.ayes) {\n    for (voter in doc.ayes) {\n      constituency = doc.ayes[voter].constituency;\n      tokens = constituency.split(/[^A-Z0-9\\-_]+/i);\n      for (word in tokens) {\n        keyword = tokens[word].toLowerCase();\n        if (words_used.indexOf(\"|\" + keyword + \"|\") < 0) {\n          if (keyword.length > 1) {\n            emit(keyword, doc._id);\n            words_used = words_used + keyword + \"|\";\n          }\n        }\n      }\n    }\n  }\n}"
+       }
+   }
+}'
+
+result = RestClient.put("#{DATABASE}/_design%2Findex", doc)
+
+p result
