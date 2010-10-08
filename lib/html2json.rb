c911b8009003699fbcc42f8692f53d8347e0329e
lizconlan <lizconlan@gmail.com>
2010-10-08 17:50:45 +0100

    rewritten the data load routine; closes gh-32

--- lib/html2json.rb
+++ lib/html2json.rb
@@ -8,10 +8,13 @@ require 'logger'
 class Html2Json
   DBSERVER = "http://localhost:5984"
   DATABASE = "#{DBSERVER}/divisionlists"
+  
+  def initialize
+    @log = Logger.new(STDOUT)
+  end
 
   def load_data files=nil
-    @log = Logger.new(STDOUT)
-    write_to_log("#{Time.now}\n")
+    @error_log = ErrorLog.new("#{File.dirname(__FILE__)}/../dataload.log")
     unless files
       paths = Dir.glob("#{File.dirname(__FILE__)}/./../data/*/*.html")
       paths += Dir.glob("#{File.dirname(__FILE__)}/./../data/*/*/*.html")
@@ -25,7 +28,7 @@ class Html2Json
         load_divisions_list(html_file)
       end
     end
-    write_to_log("\n")
+    @error_log.close()
   end
   
   private
@@ -35,8 +38,6 @@ class Html2Json
 
       @log.info("opening file: " + html_file)
       nokogiri_doc.xpath('//div[@class="division-list"]').each do |division_list|
-        error_occured = false
-    
         division_hash = {}
   
         current_file = html_file.split("/").last
@@ -52,86 +53,117 @@ class Html2Json
         error_ref = ""
     
         unless do_error_check(page, date, number, resolution, ayes, noes, current_file)
+          error_occured = false
+          
+          divnum = number.text.gsub("l","1").gsub(/[^\d+]/,'')
           #use the filename (minus the extension) plus the division number as the document id
-          uuid = "#{html_file[html_file.rindex('/')+1..html_file.length].gsub('.html','')}-#{number.text.gsub('Numb','')}"
+          uuid = "#{html_file[html_file.rindex('/')+1..html_file.length].gsub('.html','')}-#{divnum}"          
           uuid = uuid.gsub('.','').gsub(' ','').gsub(',','').gsub('data/', '').gsub('/','')
           
           division_hash["page"] = page.text
-          division_hash["date"] = date.text
+          
+          if date.count > 1
+            division_hash["date"] = date.first.text
+          else
+            division_hash["date"] = date.text
+          end
       
           #construct the date values
-          escaped_text = HTMLEntities.new.encode(date.text)
-          escaped_text.gsub!("\302\260", "")
-      
-          date_parts = escaped_text.split(",")
-      
-          year = date_parts.pop.gsub(".", "").strip
-          
-          unless year.length == 4
-            #something's wrong here!
-            parts = year.split(" ")
-            year = parts.pop.gsub(".", "").strip
-            date_parts << parts.join(" ")
-          end
-      
-          month_day = date_parts.pop.split(" ")
-          month = month_day.pop.strip
-          month = get_month_num(month)
-          if month.nil?
-            #data may be missing, imply it from the uuid
-            parts = uuid.split("-")
-            month = parts[1].to_i
-          end
-          
-          day = month_day.join(" ").match(/\d+/).to_s
-          if day.to_i == 0
-            #data may be missing, imply it from the uuid
-            parts = uuid.split("-")
-            day = parts[2]
-          end
+          year, month, day = construct_date_values(division_hash["date"], uuid, divnum, current_file)
 
           division_hash["numeric_date"] = [year.to_i, month, day.to_i]
       
-          division_hash["number"] = number.text
+          division_hash["number"] = divnum
           division_hash["resolution"] = resolution.text
       
           ayes_array = []
-          ayes.xpath('./li').each do |aye|
-            member_hash = {}
-            vote_name = VoteName.new(aye.text.strip)
-            member_hash["parliamentary_title"] = vote_name.parliamentary_title
-            member_hash["title"] = vote_name.title
-            member_hash["forename"] = vote_name.forename
-            member_hash["surname"] = vote_name.surname
-            member_hash["constituency"] = vote_name.constituency
-            member_hash["division_name"] = aye.text.strip
-            ayes_array << member_hash
+          
+          ayes_markup_fix = false
+          if ayes.count == 0
+            message = "division #{divnum} - ayes data missing"
+            if noes.count == 2
+              message = "#{message}, assuming the first noes block is in fact ayes"
+              write_to_log(current_file, message)
+              ayes = division_list.xpath('ol[@class="noes"]')[0]
+              noes = division_list.xpath('ol[@class="noes"]')[1]
+              ayes_markup_fix = true
+            else
+              write_to_log(current_file, message, "error")
+              error_occured = true
+            end
           end
-          division_hash["ayes"] = ayes_array
-      
-          ayes_first = ayes.first
-          if ayes_first
-            division_hash["ayes_tellers"] = ayes_first.next_sibling().text
+          
+          if noes.count == 0
+            message = "division #{divnum} - noes data missing"
+            write_to_log(current_file, message, "error")
+            error_occured = true
           end
-
-          noes_array = []
-          noes.xpath('./li').each do |noe|
-            member_hash = {}
-            vote_name = VoteName.new(noe.text.strip)
-            member_hash["parliamentary_title"] = vote_name.parliamentary_title
-            member_hash["title"] = vote_name.title
-            member_hash["forename"] = vote_name.forename
-            member_hash["surname"] = vote_name.surname
-            member_hash["constituency"] = vote_name.constituency
-            member_hash["division_name"] = noe.text.strip
-            noes_array << member_hash
-          end
-          division_hash["noes"] = noes_array
-
-          noes_first = noes.first
-          if noes_first
-            if noes_first.next_sibling()
-              division_hash["noes_tellers"] =  noes_first.next_sibling().text
+          
+          unless error_occured
+            ayes.xpath('./li').each do |aye|
+              if aye.text.strip == ""
+                message = "division #{divnum} - member name \"#{aye.text.strip}\" missing, member data not loaded"
+                write_to_log(current_file, message)
+              else
+                unless aye.text.strip.include?(",")
+                  message = "division #{divnum} - member name \"#{aye.text.strip}\" does not include a comma, attempting to load anyway"
+                  write_to_log(current_file, message)
+                end
+                member_hash = {}
+                vote_name = VoteName.new(aye.text.strip)
+                member_hash["parliamentary_title"] = vote_name.parliamentary_title
+                member_hash["title"] = vote_name.title
+                member_hash["forename"] = vote_name.forename
+                member_hash["surname"] = vote_name.surname
+                member_hash["constituency"] = vote_name.constituency
+                member_hash["division_name"] = aye.text.strip
+                ayes_array << member_hash
+              end
+              division_hash["ayes"] = ayes_array
+            end
+            
+            if ayes_markup_fix
+              division_hash["ayes_tellers"] = ayes.next_sibling().text
+            else            
+              ayes_first = ayes.first
+              if ayes_first
+                division_hash["ayes_tellers"] = ayes_first.next_sibling().text
+              end
+            end
+            
+            noes_array = []
+            
+            noes.xpath('./li').each do |noe|
+              if noe.text.strip == ""
+                message = "division #{divnum} - member name \"#{noe.text.strip}\" missing, member data not loaded"
+                write_to_log(current_file, message)
+              else
+                unless noe.text.strip.include?(",")
+                  message = "division #{divnum} - member name \"#{noe.text.strip}\" does not include a comma, attempting to load anyway"
+                  write_to_log(current_file, message)
+                end
+                member_hash = {}
+                vote_name = VoteName.new(noe.text.strip)
+                member_hash["parliamentary_title"] = vote_name.parliamentary_title
+                member_hash["title"] = vote_name.title
+                member_hash["forename"] = vote_name.forename
+                member_hash["surname"] = vote_name.surname
+                member_hash["constituency"] = vote_name.constituency
+                member_hash["division_name"] = noe.text.strip
+                noes_array << member_hash
+              end
+              division_hash["noes"] = noes_array
+            end
+            
+            if ayes_markup_fix
+              division_hash["noes_tellers"] =  noes.next_sibling().text
+            else
+              noes_first = noes.first
+              if noes_first
+                if noes_first.next_sibling()
+                  division_hash["noes_tellers"] =  noes_first.next_sibling().text
+                end
+              end
             end
           end
 
@@ -153,14 +185,14 @@ class Html2Json
       end
     end
 
-    def write_to_log message
-      File.open("#{File.dirname(__FILE__)}/../dataload.log", 'a') {|f| f.write(message) }
+    def write_to_log current_file, message, level="warn"
+      @error_log.log_error(current_file, message, level)
       @log.info("writing dataload.log file")
     end
 
     def log_error current_file, ref, item_name, item
-      message = "error in file #{current_file}#{ref} - #{item_name} count was #{item.count}, expected 1\n"
-      write_to_log(message)  
+      message = "#{ref} - #{item_name} count was #{item.count}, expected 1"
+      write_to_log(current_file, message)  
       @log.info(message)
     end
 
@@ -196,74 +228,175 @@ class Html2Json
     def do_error_check page, date, number, resolution, ayes, noes, current_file
       if number.count == 1
         #we can have more detailed error messages, yay!
-        error_ref = " in divisionlist #{number.text}"
+        error_ref = "division #{number.text} - "
       elsif page.count == 1
         #failover reference point
-        error_ref = " referring to page #{page.text}"
+        error_ref = "page #{page.text} - "
       else
         #no idea where this happened
         error_ref = ""
-      end    
+      end
   
-      unless page.count == 1
-        log_error current_file, error_ref, "page", page
-        error_occured = true
+      unless page.count > 0
+        message = "#{error_ref}found #{page.count} pages, expected at least 1"
+        write_to_log(current_file, message)
       end
   
       unless date.count == 1
-        log_error current_file, error_ref, "date", date
-        error_occured = true
+        message = "#{error_ref}found #{date.count} dates, expected to find 1"
+        write_to_log(current_file, message)
       end
   
-      unless number.count == 1
-        log_error current_file, error_ref, "number", number
+      if number.count < 1
+        unless error_ref == ""
+          message = "Division at #{error_ref}no division number"
+        else
+          message = "No division number found"
+        end
+        write_to_log(current_file, message, "error")
         error_occured = true
       else
-        divnum = number.text.gsub("Numb", "").gsub(".", "").gsub(",", "").strip
+        divnum = number.text.squeeze(" ").gsub("Number", "").gsub("Numb","").gsub("Namb","").gsub("Num","").gsub("???umb", "").gsub(".", "").gsub(",", "").strip
         unless divnum =~ /^\d+$/
-          message = "error in file #{current_file}#{error_ref} - division number #{divnum} may be invalid\n"
-          write_to_log message
-          error_occured = true
-        end
-      end
-  
-      unless ayes.count == 1
-        log_error current_file, error_ref, "ayes", ayes
-        error_occured = true
-      else
-        ayes.xpath('./li').each do |member|
-          unless member.text.include?(",")
-            message = "error in file #{current_file}#{error_ref} - member name #{member.text.strip} did not include a comma\n"
-            write_to_log message
+          divnum = divnum.gsub("l", "1")
+          unless error_ref == ""
+            message = "Division at #{error_ref}division number not numeric"
+          else
+            message = "Division number #{divnum} not numeric"
+          end
+          if divnum =~ /^\d+$/
+            message = "#{message}, replaced with #{divnum}"
+            number = divnum
+            write_to_log(current_file, message)
+          else
+            write_to_log(current_file, message, "error")
             error_occured = true
           end
         end
       end
   
-      unless noes.count == 1
-        log_error current_file, error_ref, "noes", noes
-        error_occured = true
-      else
-        noes.xpath('./li').each do |member|
-          unless member.text.include?(",")
-            message = "error in file #{current_file}#{error_ref} - member name #{member.text.strip} did not include a comma\n"
-            write_to_log message
-            error_occured = true
-          end
-        end
-      end
-  
-      unless resolution.count == 1
-        log_error current_file, error_ref, "resolution", resolution
+      if resolution.count < 1
+        message = "#{error_ref}no resolution text found"
+        write_to_log(current_file, message, "error")
         error_occured = true
       else
         if resolution.text.include?("???")
-          message = "error in file #{current_file}#{error_ref} - resolution text contains invalid characters\n"
-          write_to_log message
-          error_occured = true
+          message = "#{error_ref}resolution text contains invalid characters"
+          write_to_log(current_file, message)
         end
       end
   
       return error_occured
     end
+    
+    def construct_date_values(date, uuid, division, current_file)
+      error_reported = false
+      message = "division #{division} - incomplete date, date implied from filename"
+      
+      escaped_text = HTMLEntities.new.encode(date)
+      escaped_text.gsub!("\302\260", "")
+  
+      date_parts = escaped_text.split(",")
+  
+      year = date_parts.pop
+      unless year.nil?
+        year = year.gsub(".", "").strip
+      end
+      
+      if year.nil?
+        #data may be missing, imply it from the uuid
+        unless error_reported 
+          write_to_log(current_file, message)
+          error_reported = true
+        end
+        parts = uuid.split("-")
+        year = parts[0].to_i
+      else          
+        unless year.length == 4
+          #something's wrong here!
+          write_to_log(current_file, "division #{division} - date format error, attempting to recover")
+          parts = year.split(" ")
+          year = parts.pop.gsub(".", "").strip
+          date_parts << parts.join(" ")
+        end
+      end
+  
+      month_day = date_parts.pop
+      if month_day.nil?
+        month = nil
+        
+        #imply day from uuid
+        unless error_reported
+          write_to_log(current_file, message)
+          error_reported = true
+        end
+        parts = uuid.split("-")
+        day = parts[2]
+      else
+        month_day = month_day.split(" ")
+        month = month_day.pop
+        if month.nil?
+          #data may be missing, imply it from the uuid
+          unless error_reported
+            write_to_log(current_file, message)
+            error_reported = true
+          end
+          parts = uuid.split("-")
+          month = parts[1].to_i
+        else
+          month.strip!
+          month = get_month_num(month)
+        end
+      end
+      
+      if month.nil?
+        #data may be missing, imply it from the uuid
+        unless error_reported
+          write_to_log(current_file, message)
+          error_reported = true
+        end
+        parts = uuid.split("-")
+        month = parts[1].to_i
+      end
+      
+      unless month_day.nil?
+        day = month_day.join(" ").match(/\d+/).to_s
+        if day.to_i == 0
+          #data may be missing, imply it from the uuid
+          unless error_reported
+            write_to_log(current_file, message)
+            error_reported = true
+          end
+          parts = uuid.split("-")
+          day = parts[2]
+        end
+      end
+      [year, month, day]
+    end
+end
+
+
+class ErrorLog
+  def initialize(log_file)
+    @data_file = ""
+    @log_file = File.open("#{log_file}", 'a')
+  end
+  
+  def log_error(data_file, message, level="warn")
+    if @data_file == ""
+      @log_file.write("**********************************\n* #{Time.now} *\n**********************************")
+    end
+    
+    unless @data_file == data_file
+      @log_file.write("\n\nFile #{data_file}:\n")
+    end
+    @data_file = data_file
+    
+    @log_file.write("  #{level.upcase()} #{message}\n")
+  end
+  
+  def close
+    @log_file.write("\n\n")
+    @log_file.close
+  end
 end
